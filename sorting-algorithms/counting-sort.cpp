7b7f5e13e68ec27ae1a753d4b7c3ed271997f46d
BaronPipistron <baronpipistron@mail.ru>
2024-02-20 16:18:12 +0300

    counting-sort

--- sorting-algorithms/counting-sort.cpp
+++ sorting-algorithms/counting-sort.cpp
@@ -1,40 +1,55 @@
-#include <cassert>
+#include <algorithm>
 #include <iostream>
+#include <type_traits>
 #include <vector>
 
-void countingSort(std::vector<int>& arr) {
-    int max = arr[0];
-    size_t size = arr.size();
-
-    for (size_t i = 1; i != size; ++i) {
-        max = (max < arr[i]) ? arr[i] : max;
+template <class _Tp>
+std::ostream& operator << (std::ostream& os, const std::vector<_Tp>& vec) {
+    for (size_t i = 0; i != vec.size(); ++i) {
+        os << vec[i] << " ";
     }
 
-    std::vector<int> helperArr(max, 0);
+    return os;
+}
+
+template <class _Tp>
+void countingSort(std::vector<_Tp>& vec) {
+    _Tp min = *std::min_element(vec.begin(), vec.end());
+    _Tp max = *std::max_element(vec.begin(), vec.end());
+
+    _Tp difference = -1 * min;
+    size_t size = vec.size();
+
+    if (max > size || std::abs(min) > size) {
+        throw std::logic_error("ERROR: MAX_ELEMENT or abs(MIN_ELEMENT) more than SIZE. Choose another sorting algorithm");
+    }
+
+    std::vector<uint8_t> counterArr(max + difference + 1, _Tp(0));
+    std::vector<_Tp> resArr(size);
 
     for (size_t i = 0; i != size; ++i) {
-        ++helperArr[arr[i]];
+        ++counterArr[vec[i] + difference];
     }
 
-    int index = 0;
-    for (size_t i = 0; i != max + 1; ++i) {
-        for (size_t j = 0; j != helperArr[i]; ++j) {
-            arr[index++] = i;
-        }
+    for (size_t i = 1; i != max + difference + 1; ++i) {
+        counterArr[i] += counterArr[i - 1];
     }
+
+    for (int i = size - 1; i >= 0; --i) {
+        resArr[counterArr[vec[i] + difference] - 1] = vec[i];
+        --counterArr[vec[i] + difference];
+    }
+    vec = resArr;
 
     return;
 }
 
 int main() {
-    std::vector<int> vec = {5, 9, 0, 14, 5, 43, 1, 4, 2};
+    std::vector<int> vec = {-1, 2, 5, -3, 6, 7, -6, 3, 4, 5, 9, 8, 3, 9, 5};
 
     countingSort(vec);
 
-    for (size_t i = 0; i != vec.size(); ++i) {
-        std::cout << vec[i] << " ";
-    }
-    std::cout << std::endl;
+    std::cout << vec << std::endl;
 
     return 0;
 }
